f1ae30e3120a1e4999079929d66e8443faa6ff19
erguotou <erguotou525@gmail.com>
2018-12-14 22:56:52 +0800

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const { YINGYUN_USERNAME, YINGYUN_PASSWORD } = process.env;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://yingyun.me/auth/login');
+  await page.type('#email', YINGYUN_USERNAME, 60);
+  await page.type('#passwd', YINGYUN_PASSWORD, 60);
+  await page.click('#login', { delay: 20 });
+  await page.waitForNavigation()
+  await page.click('.usercheck .btn', { delay: 22 })
+  await page.waitFor(2000);
+  await browser.close();
+})();

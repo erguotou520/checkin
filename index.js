b87cae79f79feeb1f203895e845e5563cc6fda77
erguotou <erguotou525@gmail.com>
2018-12-24 11:24:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const puppeteer = require('puppeteer');
   await page.type('#email', YINGYUN_USERNAME, 60);
   await page.type('#passwd', YINGYUN_PASSWORD, 60);
   await page.click('#login', { delay: 20 });
-  await page.waitForNavigation()
-  await page.click('#checkin', { delay: 22 })
+  await page.waitForNavigation({ timeout: 600000 });
+  await page.click('#checkin', { delay: 22 });
   await page.waitFor(2000);
   await browser.close();
 })();

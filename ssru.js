c19a56b9c48c6a698c5eec4c77a70f7c00b5ecc9
erguotou <erguotou525@gmail.com>
2019-05-18 15:58:55 +0800

    update

--- ssru.js
+++ ssru.js
@@ -2,13 +2,14 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const { SSRU_USERNAME, SSRU_PASSWORD } = process.env;
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://ssru.men/auth/login');
-  await page.type('#email', SSRU_USERNAME, 60);
-  await page.type('#passwd', SSRU_PASSWORD, 60);
+  await page.type('#email', SSRU_USERNAME, { delay: 60 });
+  await page.type('#passwd', SSRU_PASSWORD, { delay: 60 });
   await page.click('#login', { delay: 20 });
   await page.waitForNavigation({ timeout: 600000 });
+  await page.waitFor(2000);
   await page.click('#result_ok', { delay: 50 });
   await page.waitFor(2000);
   await page.click('#checkin', { delay: 22 });
